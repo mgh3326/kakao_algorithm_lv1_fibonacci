cd0b38b20fc96d6d9a05ec47495e2ab53280d126
mgh3326 <mgh3326@naver.com>
2018-01-01 17:25:38 +0900

    complete

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,10 +1,24 @@
-/*
- * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
 
-public class fibonacci {
+class Fibonacci {
+    long[] memo = new long[100];//static 해줘야되나?
+    public long fibonacci(int n) {
+        //long answer = 0;
+        if (n <= 1) {
+            return n;
+        }
+        else if (memo[n] != 0) {
+            return memo[n];
+        }
+        else {
+            return memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        }
+        //return answer;
+    }
+
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String[] args) {
+        Fibonacci c = new Fibonacci();
+        int testCase = 3;
+        System.out.println(c.fibonacci(testCase));
+    }
 }
